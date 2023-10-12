8218b98534351bdf83bd15a939ac6b7a3dafbdf7
victoria <132116126+vliu12@users.noreply.github.com>
2023-10-12 12:51:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 
-for (var i = 1; i <= 7; i++) {
+for (var i = 1; i <= 10; i++) {
   var thumb = document.createElement("img");
   thumb.src = "images/image" + i + ".JPG";
   thumb.alt = "Image " + i;
